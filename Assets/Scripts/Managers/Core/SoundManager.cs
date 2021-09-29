1e5c8eb0fe1c0e4e7a00dff1637174a27ac6859b
DodoBunny <dodosae98@naver.com>
2021-09-29 18:59:42 +0900

    soundManager

---
+++ Assets/Scripts/Managers/Core/SoundManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager
+{
+    public AudioClip BGM_clip;
+    public AudioClip Food_clip;
+    public AudioClip Drink_clip;
+    public AudioClip Book_clip;
+
+    public AudioSource BGM;
+    public AudioSource Item;
+
+
+    public void Init()
+    {
+        BGM = GameObject.Find("BGM").GetComponent<AudioSource>();
+        Item = GameObject.Find("Item").GetComponent<AudioSource>();
+
+        BGM_clip = Resources.Load("Sounds/Music/BGM") as AudioClip;
+        Food_clip = Resources.Load("Sounds/SFX/Food") as AudioClip;
+        Drink_clip = Resources.Load("Sounds/SFX/Drink") as AudioClip;
+        Book_clip = Resources.Load("Sounds/SFX/Book") as AudioClip;
+
+        BGM.clip = BGM_clip;
+        BGM.Play();
+        BGM.loop = true;
+}
+    
+    public void Play(Define.Sounds soundName)
+    {
+        switch (soundName)
+        {
+            case Define.Sounds.Book:
+                Item.PlayOneShot(Book_clip);
+                break;
+            case Define.Sounds.Drink:
+                Item.PlayOneShot(Drink_clip);
+                break;
+            case Define.Sounds.Food:
+                Item.PlayOneShot(Food_clip);
+                break;
+        }
+    } 
+}
--- Assets/Scripts/Utils/Define.cs
+++ Assets/Scripts/Utils/Define.cs
@@ -7,4 +7,11 @@ public class Define
     /*
         TODO : enum, String같은 고정 변수들 정의하는 스크립트 
     */
+    public enum Sounds
+    {
+        BGM,
+        Food,
+        Drink,
+        Book
+    }
 }
