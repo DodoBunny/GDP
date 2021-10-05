d34cc6107feab0f52253c9c7a211355cf5e28f3c
JUNYEOL-GONG <gongjunyeol@gmail.com>
2021-10-05 18:10:10 +0900

    다이얼로그, 무빙플랫폼 완성
    프리팹으로 옮기기
    스크립트 수정(대화창 ray아닌 상태에서도 없어지게)

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,17 +9,27 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D _rigid;
     Animator _anim;
     SpriteRenderer _sprite;
+    AudioSource _audio; // 점프 효과음
+
+    public float moveSpeed = 10f;
+    public float jumpPower = 20f;
+    public float maxSpeed = 20f;
 
     [SerializeField]
-    float maxSpeed;
-    [SerializeField]
-    float moveSpeed = 10f;
-    [SerializeField]
-    float jumpPower = 20f;
+    private bool isGrounded = false;
 
-    [SerializeField]
-    bool isGrounded = false;
+    public float distance;
+    public float angle;
 
+    LayerMask groundMask;
+    public Transform chkPos;
+    public float checkRadius;
+
+    // Dialogue
+    Vector3 dirVec; // 단위 벡터 (방향을 알기위함)
+    float h;
+    GameObject scanObject; // 스캔할 오브젝트
+    public DialogueManager D_manager; // 다이얼로그메니저를 가져온다 (Action함수를 가져오기 위함)
 
     private void Awake()
     {
@@ -26,18 +37,24 @@ public class PlayerController : MonoBehaviour
         _rigid = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
         _sprite = GetComponent<SpriteRenderer>();
+        _audio = GetComponent<AudioSource>();
+        groundMask = LayerMask.GetMask("Ground");
     }
 
     // Update is called once per frame
     void Update()
     {
-        GroundedCheck();
+        Direction();
+        Scan();
         Jump();
         SetAnim();
+        GroundChk();
     }
+
     private void FixedUpdate()
     {
         Move();
+        Ray();
     }
 
     void Move()
@@ -46,8 +63,17 @@ public class PlayerController : MonoBehaviour
 
         if (_rigid.velocity.x > maxSpeed)
             _rigid.velocity = new Vector2(maxSpeed, _rigid.velocity.y);
-        else if (_rigid.velocity.x < -maxSpeed)
-            _rigid.velocity = new Vector2(-maxSpeed, _rigid.velocity.y);
+        else if (_rigid.velocity.x < maxSpeed * (-1))
+            _rigid.velocity = new Vector2(maxSpeed * (-1), _rigid.velocity.y);
+
+        //키 입력 없을 때 이동 제한
+        //if (Managers.Input.horizontal == 0 && isGrounded)
+            //_rigid.velocity = new Vector2(0, _rigid.velocity.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distance, groundMask);
+        angle = Vector2.Angle(hit.normal, Vector2.up);
+        Debug.DrawLine(hit.point, hit.point + hit.normal, Color.blue);
+
 
     }
 
@@ -59,21 +85,52 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            _audio.Play(); // 점프 효과음 재생
             _anim.SetTrigger("Jump");
         }
     }
 
-    void GroundedCheck()
+    void Direction()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, LayerMask.GetMask("Ground"));
-        Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.blue, 0.5f);
-        if (hit.transform != null)
+        h = Input.GetAxisRaw("Horizontal");
+
+        // Direction
+        if (h == 1) // 오른쪽 방향키를 누르면 1이 입력됨
         {
-            isGrounded = true;
+            dirVec = Vector3.right; // dirVec에 오른쪽 방향을 넣어준다.
+        }
+        else if (h == -1) // 왼쪽 방향키를 누르면 -1이 입력됨
+        {
+            dirVec = Vector3.left; // dirVec에 왼쪽 방향을 넣어준다.
+        }
+
+    }
+
+    void Ray() // 오브젝트를 스캔한다.
+    {
+        Debug.DrawRay(_rigid.position, dirVec * 0.5f, new Color(0, 1, 0)); // Ray 그리기
+        RaycastHit2D rayHit = Physics2D.Raycast(_rigid.position, dirVec, 0.5f, LayerMask.GetMask("Dialogue")); // Raycast 효과 넣기
+
+        if (rayHit.collider != null) // Ray에 닿는 콜라이더가 있다면
+        {
+            scanObject = rayHit.collider.gameObject; // scanObject에 그 콜라이더 오브젝트를 넣어준다.
         }
         else
         {
-            isGrounded = false;
+            scanObject = null; // scanObject에 null을 넣어준다.
+        }
+
+    }
+
+    void Scan() // v키(Interaction)을 누르면 함수 실행
+    {
+        if (Input.GetButtonDown("Interaction") && scanObject != null) // v키를 누르고 scanObject가 있다면
+        {
+            D_manager.Action(scanObject); // Action함수 실행
+        }
+        else if(Input.GetButtonDown("Interaction") && scanObject == null) // v키를 누르고 scanObject가 없다면
+        {
+            D_manager.DialoguePanel.SetActive(false); // 판넬을 없앤다
         }
     }
 
@@ -90,8 +147,42 @@ public class PlayerController : MonoBehaviour
             _sprite.flipX = false;
         }
         else
+        {
             _anim.SetBool("IsMove", false);
+        }
+
 
         _anim.SetBool("IsGrounded", isGrounded);
+
+        if (_rigid.velocity.y <= -3f && isGrounded == false)
+            _anim.Play("Falling");
     }
+
+    void GroundChk()
+    {
+        isGrounded = Physics2D.OverlapCircle(chkPos.position, checkRadius, groundMask);
+    }
+   
+    /*
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        // 바닥에 닿았음을 감지하는 처리
+        // 어떤 콜라이더와 닿았으며, 충돌 표면이 위쪽을 보고 있으면
+
+            for (int i = 0; i < collision.contactCount; i++)
+            {
+            if (collision.contacts[i].normal.y > 0.7f)
+                if (isGrounded == false)
+                    _anim.Play("Idle");
+                isGrounded = true;
+            }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // 바닥에서 벗어났음을 감지하는 처리
+        isGrounded = false;
+    }
+    */
+
 }
---
+++ Assets/Scripts/UI/DialogueManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject DialoguePanel; // 판넬 오브젝트를 가져옴
+    public Text Dialogue; // 텍스트 오브젝트
+    public GameObject scanObject; // 스캔할 오브젝트
+    public bool isAction; // Action 함수의 부울
+
+    public void Action(GameObject scanObj)
+    {
+        if(isAction) // 판넬이 켜져있으면
+        {
+            isAction = false; // 판넬에 false값 준다
+        }
+        else // 판넬이 꺼져있으면
+        {
+            isAction = true; // 판넬에 true값 준다
+            Dialogue.text = "안녕! 가천대학교에 온걸 환영해"; // 텍스트를 넣어준다
+            //scanObject = scanObj;
+
+        }
+
+        DialoguePanel.SetActive(isAction); // 판넬의 부울값에 따라 판넬을 키고 끈다.
+    }
+
+}
+
+
