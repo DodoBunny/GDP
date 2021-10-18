e9b70311ed019ac7ddd13fc58431836e5da63497
DodoBunny <dodosae98@naver.com>
2021-10-19 00:48:19 +0900

    .

--- Assets/Scripts/Controllers/NPCController.cs
+++ Assets/Scripts/Controllers/NPCController.cs
@@ -6,56 +6,95 @@ public class NPCController : MonoBehaviour
 {
     Animator _anim;
     SpriteRenderer _sprite;
+    RaycastHit2D hit;
 
-    public float sight = 10f;
+    public float sight = 6f;
     public float speed = 3f;
 
     bool targetLock = false;
+    Vector3 patrolDir;
+    enum State
+    {
+        Patrol,
+        Follow,
+        Attack
+    }
+    State state = State.Patrol;
 
     // Start is called before the first frame update
     void Start()
     {
         _anim = GetComponent<Animator>();
         _sprite = GetComponent<SpriteRenderer>();
+        InvokeRepeating("RandomPos", 0, Random.Range(3f, 7f));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Managers.Game.player.transform.position.y == transform.position.y)
-            targetLock = true;
-        else
-            targetLock = false;
-
+        Ray();
         if (!targetLock)
         {
             if (Mathf.Abs((Managers.Game.player.transform.position - transform.position).magnitude) < sight)
             {
-                _anim.SetBool("IsMove", true);
-                Vector3 dir = -(transform.position - Managers.Game.player.transform.position).normalized;
-                transform.position += dir * speed * Time.deltaTime;
+                if (hit.collider == null)
+                    state = State.Follow;
+                else
+                    state = State.Attack;
             }
             else
             {
-                _anim.SetBool("IsMove", false);
+                state = State.Patrol;
             }
         }
 
+        switch (state)
+        {
+            case State.Patrol:
+                _anim.SetBool("IsMove", true);
+                if (patrolDir.x < 0)
+                {
+                    _sprite.flipX = true;
+                }
+                else
+                {
+                    _sprite.flipX = false;
+                }
+                transform.position += patrolDir * speed * Time.deltaTime;
+                break;
+            case State.Follow:
+                _anim.SetBool("IsMove", true);
+                if (transform.position.x > Managers.Game.player.transform.position.x)
+                {
+                    _sprite.flipX = true;
+                }
+                else
+                {
+                    _sprite.flipX = false;
+                }
+                Vector3 dir = -(transform.position - Managers.Game.player.transform.position).normalized;
+                transform.position += dir * speed * Time.deltaTime;
+                break;
+            case State.Attack:
+                _anim.SetBool("IsMove", false);
+                break;
+        }
 
-        if (transform.position.x > Managers.Game.player.transform.position.x)
-        {
-            _sprite.flipX = true;
-        }
-        else
-        {
-            _sprite.flipX = false;
-        }
 
     }
 
-    void Patrol()
+    void Ray()
     {
+        Vector3 Dir = transform.position.x > Managers.Game.player.transform.position.x ? Vector3.left : Vector3.right;
 
+        Debug.DrawRay(transform.position, Dir);
+        hit = Physics2D.Raycast(transform.position, Dir, 6f, LayerMask.GetMask("Player"));
+    }
+
+    void RandomPos()
+    {
+        Vector3 dir = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0).normalized;
+        patrolDir = dir;
     }
 
 }
