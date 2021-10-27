0ce9148d0aeebe0c45f0c6a92ee24595e0076d76
JUNYEOL-GONG <gongjunyeol@gmail.com>
2021-10-27 21:25:18 +0900

    메인 메뉴 틀
    메인 메뉴 점수랑 사운드 기능은 아직 못넣음

--- Assets/Scripts/Scenes/LobbyScene.cs
+++ Assets/Scripts/Scenes/LobbyScene.cs
@@ -1,26 +1,122 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class LobbyScene : MonoBehaviour
+
+public enum BTNTpye // 열거형
 {
-    // Update is called once per frame
-    void Update()
+    Start,
+    Score,
+    Option,
+    Sound,
+    Back,
+    Exit
+}
+
+
+public class LobbyScene : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public BTNTpye currentType; // enum
+    public Transform buttonScale;
+    Vector3 defaultScale;
+    public CanvasGroup mainGroup;
+    public CanvasGroup optionGroup;
+    bool isSound;
+    public Text text;
+
+
+    private void Start()
     {
-        
+        defaultScale = buttonScale.localScale;
+        isSound = true;
     }
 
-    public void GameStart()
+
+    public void OnBtnClick()
     {
-        SceneManager.LoadScene("Stage 1 - 1");
-    }
-    public void GameExit()
-    {
+        switch (currentType)
+        {
+            case BTNTpye.Start:
+                Debug.Log("게임 시작");
+                SceneManager.LoadScene("Stage 1 - 1");
+                break;
+
+            case BTNTpye.Score:
+                Debug.Log("점수");
+                break;
+
+            case BTNTpye.Option:
+                Debug.Log("옵션");
+                OnCanvasGroupChanged(optionGroup);
+                OffCanvasGroupChanged(mainGroup);
+                break;
+
+            case BTNTpye.Sound:
+                Debug.Log("사운드");
+                if(isSound)
+                {
+                    Debug.Log("사운드 OFF");
+                    text.text = "사운드 OFF";
+                }
+                else
+                {
+                    Debug.Log("사운드 ON");
+                    text.text = "사운드 ON";
+                }
+
+                isSound = !isSound;
+                //Managers.Sound.BGM_clip
+                break;
+
+            case BTNTpye.Back:
+                Debug.Log("뒤로");
+                OnCanvasGroupChanged(mainGroup);
+                OffCanvasGroupChanged(optionGroup);
+                break;
+
+            case BTNTpye.Exit:
+                Debug.Log("게임 종료");
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+                UnityEditor.EditorApplication.isPlaying = false;
 #endif
-        Application.Quit();
+                Application.Quit();
+                break;
+
+
+
+
+        }
+
     }
 
+    public void OnCanvasGroupChanged(CanvasGroup cg)
+    {
+        cg.alpha = 1;
+        cg.interactable = true;
+        cg.blocksRaycasts = true;
+    }
+
+
+    public void OffCanvasGroupChanged(CanvasGroup cg)
+    {
+        cg.alpha = 0;
+        cg.interactable = false;
+        cg.blocksRaycasts = false;
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale * 1.2f;
+
+    }
+
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale;
+    }
 }
