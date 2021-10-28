fc5bd0986409667667321f715711a49591b1dd03
JUNYEOL-GONG <gongjunyeol@gmail.com>
2021-10-29 05:10:58 +0900

    재업

--- Assets/Scripts/Scenes/LobbyScene.cs
+++ Assets/Scripts/Scenes/LobbyScene.cs
@@ -57,12 +57,12 @@ public class LobbyScene : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
             case BTNTpye.Sound:
                 if(isSound)
                 {
-                    Debug.Log("사운드 OFF");
+                    Debug.Log("사운드 ON");
                     text.text = "사운드 OFF";
                 }
                 else
                 {
-                    Debug.Log("사운드 ON");
+                    Debug.Log("사운드 OFF");
                     text.text = "사운드 ON";
                 }
 
