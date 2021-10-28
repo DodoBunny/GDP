398c5db5b69f20ba895f9447c3f7d698c1a4be95
DodoBunny <dodosae98@naver.com>
2021-10-28 22:40:54 +0900

    .

---
+++ Assets/Scripts/Contents/Stage3/Order.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Order : MonoBehaviour
+{
+    public GameObject OrderUI;
+    public GameObject Beer, Chicken, Soju;
+    public Define.Food[] inven = new Define.Food[8];
+    public GameObject[] items = new GameObject[8];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < inven.Length; i++)
+        {
+            inven[i] = Define.Food.Empty;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < inven.Length; i++)
+        {
+            if (inven[i] == Define.Food.Empty && items[i] != null)
+            {
+                Destroy(items[i]);
+            }
+        }
+
+        for (int i = 0; i < inven.Length; i++)
+        {
+            if (inven[i] == Define.Food.Empty)
+            {
+                OrderUI.SetActive(false);
+            }
+            else
+            {
+                OrderUI.SetActive(true);
+                break;
+            }
+        }
+    }
+
+    public void Add(Define.Food food)
+    {
+        if (food == Define.Food.Empty)
+            DeleteAll();
+
+        for (int i = 0; i < inven.Length; i++)
+        {
+            if (inven[i] != Define.Food.Empty)
+            {
+                continue;
+            }
+            else
+            {
+                switch (food)
+                {
+                    case Define.Food.Beer:
+                        inven[i] = Define.Food.Beer;
+                        items[i] = Instantiate(Beer, OrderUI.transform);
+                        break;
+
+                    case Define.Food.Chicken:
+                        inven[i] = Define.Food.Chicken;
+                        items[i] = Instantiate(Chicken, OrderUI.transform);
+                        break;
+
+                    case Define.Food.Soju:
+                        inven[i] = Define.Food.Soju;
+                        items[i] = Instantiate(Soju, OrderUI.transform);
+                        break;
+                }
+                break;
+            }
+        }
+    }
+
+    public void DeleteAll()
+    {
+        for (int i = 0; i < inven.Length; i++)
+        {
+            inven[i] = Define.Food.Empty;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Contents/Stage3/Table.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour
+{
+    public GameObject OrderCanvas;
+
+    public Sprite empty;
+    public Sprite wait;
+    public Sprite eat;
+
+    SpriteRenderer sprite;
+    Order order;
+
+
+    public enum State
+    {
+        Empty,
+        Wait,
+        Eat
+    }
+    State state = State.Empty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        order = OrderCanvas.GetComponent<Order>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case State.Empty:
+                sprite.sprite = empty;
+                if (!onEmpty)
+                {
+                    Invoke("Empty", Random.Range(0f, 5f));
+                    onEmpty = true;
+                }
+                break;
+            case State.Wait:
+                sprite.sprite = wait;
+                if (!onWait)
+                {
+                    Wait();
+                    onWait = true;
+                }
+                break;
+            case State.Eat:
+                sprite.sprite = eat;
+                break;
+        }
+    }
+
+    bool onEmpty = false;
+    void Empty()
+    {
+        state = State.Wait;
+    }
+
+    bool onWait = false;
+    void Wait()
+    {
+        for (int i = 0; i < Random.Range(4, 8); i++)
+        {
+            int randint = Random.Range(0, 3);
+            switch (randint)
+            {
+                case 0:
+                    order.Add(Define.Food.Beer);
+                    break;
+                case 1:
+                    order.Add(Define.Food.Chicken);
+                    break;
+                case 2:
+                    order.Add(Define.Food.Soju);
+                    break;
+            }
+        }
+    }
+}
